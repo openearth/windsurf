4724b9bceeb753d60e8540be61dafd94d6c6c7e8
Bas Hoonhout <bas@hoonhout.com>
2015-07-10 13:16:12 +0200

    Initial Python package and documentation. Both empty.

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. windsurf documentation master file, created by
+   sphinx-quickstart on Fri Jul 10 13:04:44 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to windsurf's documentation!
+====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='windsurf',
+    version='0.0',
+    author='Bas Hoonhout',
+    author_email='b.m.hoonhout@tudelft.nl',
+    packages=find_packages(),
+    description='A composite model for simulating integrated nearshore and aeolian sediment transport',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'numpy',
+        'docopt',
+    ],
+    setup_requires=[
+        'sphinx',
+        'sphinx_rtd_theme'
+    ],
+    tests_require=[
+        'nose'
+    ],
+    test_suite='nose.collector',
+    entry_points={'console_scripts': [
+        '{0} = windsurf.cmd:main'.format(
+            'windsurf'),
+    ]},
+)
